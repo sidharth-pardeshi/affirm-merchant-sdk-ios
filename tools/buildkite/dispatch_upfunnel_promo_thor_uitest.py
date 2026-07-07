afaa1b2f7a2ff9abc6705ae12455efc593db2baf
Sidharth Pardeshi <sidharth.pardeshi@affirm.com>
2026-07-07 16:59:45 -0700

    Revert "Match Affirm GitHub API auth scheme for iOS Thor dispatch"
    This reverts commit e672c528b0251dcdbc041399bb84f90d1cfd09e3.

--- tools/buildkite/dispatch_upfunnel_promo_thor_uitest.py
+++ tools/buildkite/dispatch_upfunnel_promo_thor_uitest.py
@@ -76,7 +76,7 @@ def github_request(
         method=method,
         headers={
             "Accept": "application/vnd.github+json",
-            "Authorization": f"token {token}",
+            "Authorization": f"Bearer {token}",
             "Content-Type": "application/json",
             "X-GitHub-Api-Version": "2022-11-28",
         },
